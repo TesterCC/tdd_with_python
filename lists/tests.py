1ae763368423a14bdf0688ac5fefe9c7da57f33c
LYX <testerlyx@foxmail.com>
2017-06-21 01:34:22 +0800

    20170621_03, add HomePageTest deliberately failing testcase.

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)   # expected failed
+# class SmokeTest(TestCase):
+    # def test_bad_maths(self):
+    #     self.assertEqual(1+1, 3)   # expected failed
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
 
 

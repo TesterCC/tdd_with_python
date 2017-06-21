7d2377ea0a118cb1f1530acce5b868ab14a8b92e
LYX <testerlyx@foxmail.com>
2017-06-22 02:14:53 +0800

    20170622_02, refactor home page view to use a template.

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -24,9 +25,11 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)  # .decode() 把response.content中的字节转换成Python中的,Unicode字符串
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))   # Didn't add .strip() works also well.
 
 
 

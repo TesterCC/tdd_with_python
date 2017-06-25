68d7239460caf069ec7b131a902f8df2bc16e69d
LYX <testerlyx@foxmail.com>
2017-06-25 19:21:59 +0800

    20170625_01, finish add one item, but need to refactor.

--- tdd_testcase/functional_tests5_p52.py
+++ tdd_testcase/functional_tests5_p52.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
 # python 3.5.2 , django 1.8
-# P34
+# P52
 
 
 from selenium import webdriver
@@ -62,9 +62,25 @@ class NewVisitorTest(unittest.TestCase):
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly"
         # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # 伊迪丝想知道这个网站是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+        # 页面中有一些文字解说这个功能
+
+        # 她访问那个URL，发现待办事项清单还在
+
+
+        # 期望的失败
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':     # python use it to check whether the script run in command line.
